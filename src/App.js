60b92512902ed40e950252db303270f01121c112
vicyarmots <victor.yarmots@yandex.ru>
2021-03-20 03:35:01 +0300

    add disable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import LoginForm from './components/LoginForm';
-import './style.css';
+import React from 'react'
+import LoginForm from './components/LoginForm'
+import './style.css'
 
 function App() {
-	return (
-		<div className="main">
-			<div className="main-content">
-				<LoginForm />
-			</div>
-		</div>
-	);
+    return (
+        <div className="main">
+            <div className="main-content">
+                <LoginForm />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,67 +1,57 @@
 import React, { useState } from 'react'
 
 const LoginForm = () => {
-     const [user, setUser] = useState({
-          email: '',
-          password: '',
-     })
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    })
 
-     const handleUserEmail = (event) => {
-          setUser((prevState) => ({
-               ...prevState,
-               email: event.target.value,
-          }))
-     }
+    const handleInputChange = ({ target }) => {
+        setUser({ ...user, [target.name]: target.value })
+    }
 
-     const handleUserPassword = (event) => {
-          setUser((prevState) => ({
-               ...prevState,
-               password: event.target.value,
-          }))
-     }
+    const disableCheck = Object.values(user).every((item) => item)
 
-     const submitUser = (event) => {
-          event.preventDefault()
-          if (user.email !== '' && user.password !== '') {
-               console.log(`Email : ${user.email}`)
-               console.log(`Password: ${user.password}`)
-          } else {
-               return
-          }
-          setUser({ email: '', password: '' })
-     }
+    const submitUser = (event) => {
+        event.preventDefault()
+        console.log(user)
+        setUser({ email: '', password: '' })
+    }
+    return (
+        <form>
+            <div className="login-form">
+                <h1>Sign In :</h1>
+                <div className="form-group">
+                    <label>Email </label>
+                    <input
+                        value={user.email}
+                        placeholder="email"
+                        onChange={handleInputChange}
+                        name="email"
+                        type="email"
+                    />
+                </div>
 
-     return (
-          <form>
-               <div className="login-form">
-                    <h1>Sign In :</h1>
-                    <div className="form-group">
-                         <lable>Email :</lable>
-                         <input
-                              value={user.email}
-                              placeholder="email"
-                              onChange={handleUserEmail}
-                              name="email"
-                              type="email"
-                         />
-                    </div>
-
-                    <div className="form-group">
-                         <lable>Password :</lable>
-                         <input
-                              value={user.password}
-                              placeholder="password"
-                              onChange={handleUserPassword}
-                              name="password"
-                              type="password"
-                         />
-                    </div>
-                    <button type="sumbit" onClick={submitUser}>
-                         Submit
-                    </button>
-               </div>
-          </form>
-     )
+                <div className="form-group">
+                    <label>Password </label>
+                    <input
+                        value={user.password}
+                        placeholder="password"
+                        onChange={handleInputChange}
+                        name="password"
+                        type="password"
+                    />
+                </div>
+                <button
+                    type="sumbit"
+                    onClick={submitUser}
+                    disabled={!disableCheck}
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    )
 }
 
 export default LoginForm
